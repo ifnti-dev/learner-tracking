13079b3d33ec810ef457959076082a45aae96782
MAKAYABA Koudema Florian <makayabaflorian2006.gmail.com>
2026-08-18 14:33:38 +0000

    rien

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,7 +9,7 @@
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
     />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Sign In| TailAdmin - Tailwind CSS Admin Dashboard Template</title>
+    <title>Sign In</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body
